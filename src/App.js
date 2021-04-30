0aa19fa826278bb490bdbbd480ddd97c20d171d8
Ryan Kim <hanjun1@ualberta.ca>
2021-04-30 14:13:22 -0600

    project 4 assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import CircleSelector from "./components/CircleSelector/CircleSelector";
+import Circles from "./components/Circles/Circles";
 
-function App() {
+function App(props) {
+  const [selected, setSelected] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
+      <main>
+        <CircleSelector selected={selected} setSelected={setSelected} />
+        <Circles selected={selected} />
+      </main>
     </div>
   );
 }
